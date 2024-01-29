8ef7b445e1f6b4f53d4d8e8e14bf1a7fb9513aeb
Nibras <nibras.sid@gmail.com>
2024-01-29 19:32:49 +0000

    port updated

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ const morgan = require('morgan');
 const cors = require('cors');
 
 const app = express();
-const port = 3000;
 
+const port = process.env.PORT || 3000;
 
 
 // Load database properties from db.properties file
